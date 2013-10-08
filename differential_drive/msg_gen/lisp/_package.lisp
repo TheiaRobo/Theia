a6318f8094b33cae2faf066e4fa280c476b2107c
robo <robo@robo-VirtualBox.(none)>
2013-10-08 15:18:36 +0200

    Removed gitignore file on differential drive. We don´t know what we are doing heelp.

---
+++ differential_drive/msg_gen/lisp/_package.lisp
@@ -0,0 +1,25 @@
+(cl:defpackage differential_drive-msg
+  (:use )
+  (:export
+   "<SPEED>"
+   "SPEED"
+   "<KEYEVENT>"
+   "KEYEVENT"
+   "<MOUSEEVENT>"
+   "MOUSEEVENT"
+   "<SHARP>"
+   "SHARP"
+   "<ANALOGC>"
+   "ANALOGC"
+   "<ODOMETRY>"
+   "ODOMETRY"
+   "<ENCODERS>"
+   "ENCODERS"
+   "<PWM>"
+   "PWM"
+   "<PARAMS>"
+   "PARAMS"
+   "<SERVOMOTORS>"
+   "SERVOMOTORS"
+  ))
+
---
+++ differential_drive/msg_gen/lisp/_package_PWM.lisp
@@ -0,0 +1,8 @@
+(cl:in-package differential_drive-msg)
+(cl:export '(HEADER-VAL
+          HEADER
+          PWM1-VAL
+          PWM1
+          PWM2-VAL
+          PWM2
+))
