eb2d218bcc091da5d5d2527eeed68b8e8c43e1d1
robo <robo@NUC.(none)>
2013-10-09 20:53:34 +0200

    Minor gain changes. Found problem with the delta_encoder readings.

---
+++ MotorControl/build/CMakeFiles/FakeMotor_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/FakeMotor_node.dir/src/FakeMotor_node.o"
+  "../bin/FakeMotor_node.pdb"
+  "../bin/FakeMotor_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/FakeMotor_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ MotorControl/build/CMakeFiles/MotorControl_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/MotorControl_node.dir/src/MotorControl_node.o"
+  "../bin/MotorControl_node.pdb"
+  "../bin/MotorControl_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/MotorControl_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
