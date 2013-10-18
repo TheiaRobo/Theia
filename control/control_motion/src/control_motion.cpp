2d65759afbb34a95fcb4c162bff86e6b0452e965
robo <robo@NUC.(none)>
2013-10-18 13:56:20 +0200

    Finished control_motion skeleton.

--- control/control_motion/src/control_motion.cpp
+++ control/control_motion/src/control_motion.cpp
@@ -6,6 +6,17 @@
 
 double freq=10.0;
 
+
+/* Function that should update the odometry values obtained from the core*/
+void odo_proc(nav_msgs::Odometry::ConstPtr odo_msg){
+
+}
+
+/* Function that should update the IR values obtained from the core*/
+void ir_proc(core_sensors::ir::ConstPtr ir_msg){
+
+}
+
 int main(int argc, char ** argv){
 
 	ros::init(argc, argv, "control_motion");
@@ -16,6 +27,9 @@ int main(int argc, char ** argv){
 	ros::Rate loop_rate(freq);
 	
 	vw_pub = n.advertise<core_control_motor::vw>("/control_motion/vw",1);
+	odo_sub = n.subscribe("/core_sensors/odometry",1,odo_proc);
+	ir_sub = n.subscribe("/core_sensors/ir",1,ir_proc);
+	
 	
 	
 	while(ros::ok()){
