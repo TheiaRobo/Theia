a596f7b8deaeb626cd317d9b8e8ee54d5764dc88
Alessandro Motta <amotta91@gmail.com>
2013-11-05 16:20:35 +0100

    Train! Hard! Harder!

--- vision/vision_object/src/main.cpp
+++ vision/vision_object/src/main.cpp
@@ -23,6 +23,7 @@ int main(int argc, char ** argv){
 
 	TrainConfig_t trainConfig;
 	trainConfig.path = trainDir;
+	trainConfig.surfMinHessian = 800;
 
 	errorCode = train(trainConfig);
 
--- vision/vision_object/src/train.cpp
+++ vision/vision_object/src/train.cpp
@@ -2,13 +2,76 @@
 #include <string>
 #include <vector>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+
 #include <vision/file.h>
 
 #include "file.h"
 #include "train.h"
 
-using namespace std;
+using namespace cv;
 
+int trainFile(
+	ObjectFileTrain_t & trainFile,
+	TrainConfig_t & trainConfig
+);
+
+int trainFile(
+	ObjectFileTrain_t & trainFile,
+	TrainConfig_t & trainConfig
+){
+	std::cout << "TRAINING" << std::endl;
+	std::cout << " Path: " << trainFile.path << std::endl;
+
+	/**
+	* Read image
+	*/
+	Mat image;
+	image = imread(trainFile.path, IMREAD_GRAYSCALE);
+
+	if(!image.data){
+		std::cout << "ERROR: Could not read image" << std::endl;
+		return -1;
+	}
+
+	/**
+	* Extract features from image
+	*/
+	SurfFeatureDetector detector(trainConfig.surfMinHessian);
+	std::vector<KeyPoint> keypoints;
+
+	detector.detect(image, keypoints);
+
+	std::cout << " Number of keypoints: " << keypoints.size() << std::endl;
+
+	/**
+	* Draw keypoints
+	*/
+	Mat imageWithKeypoints;
+	drawKeypoints(
+		image,
+		keypoints,
+		imageWithKeypoints,
+		Scalar::all(-1),
+		DrawMatchesFlags::DRAW_RICH_KEYPOINTS
+	);
+
+	imshow("Keypoints", imageWithKeypoints);
+	waitKey(0);
+
+	/**
+	* Calculate corresponding descriptors
+	*/
+	SurfDescriptorExtractor extractor;
+	Mat descriptors;
+
+	extractor.compute(image, keypoints, descriptors);
+
+	return 0;
+}
 
 /**
 * Find and list training files
@@ -19,24 +82,36 @@ int train(TrainConfig_t & trainConfig){
 	/**
 	* Find files in training directory
 	*/
-	vector<string> fileVect;
+	std::vector<std::string> fileVect;
 	errorCode = visionFileScanDir(trainConfig.path, fileVect);
 
 	if(errorCode){
-		cout << "Error: Could not scan training directory" << endl;
+		std::cout << "Error: Could not scan training directory" << std::endl;
 		return errorCode;
 	}
 
 	/**
 	* Filter training files
 	*/
-	vector<ObjectFileTrain_t> trainFileVect;
+	std::vector<ObjectFileTrain_t> trainFileVect;
 	objectFilesFilter(fileVect, trainFileVect);
 	objectFilesShow(trainFileVect);
 
 	/**
 	* Extract features from training images
 	*/
+	size_t numbTrainFiles;
+	numbTrainFiles = trainFileVect.size();
+
+	size_t i;
+	for(i = 0; i < numbTrainFiles; i++){
+		errorCode = trainFile(trainFileVect[i], trainConfig);
+
+		if(errorCode){
+			std::cout << "Error: Could not train image" << std::endl;
+			return errorCode;
+		}
+	}
 
 	return 0;
 }
