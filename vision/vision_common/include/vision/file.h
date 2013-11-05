2b646f006597ad74d2799f6ce3a3117c7e365f7f
Alessandro Motta <amotta91@gmail.com>
2013-11-05 13:50:28 +0100

    Follow up to last commit Remove filtering from list of exported function.

--- vision/vision_common/include/vision/file.h
+++ vision/vision_common/include/vision/file.h
@@ -4,26 +4,12 @@
 #include <string>
 #include <vector>
 
-typedef struct {
-    std::string path;
-    std::string objectName;
-    int angle;
-} VisionTrainingFile_t;
-
 /**
 * Recursively scan a directory for files
 */
 int visionFileScanDir(
 	std::string dirName,
 	std::vector<std::string> & fileVect
-);
-
-/**
-* Find training files from file list
-*/
-int visionFileFilterTraining(
-	std::vector<std::string> & fileVect,
-	std::vector<VisionTrainingFile_t> & trainingFileVect
 );
 
 #endif
