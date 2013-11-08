df9af4a47ed1841fd0e9d30ab633e8b9639b194e
Alessandro Motta <amotta91@gmail.com>
2013-11-08 10:31:21 +0100

    New datatype for recognition score This way we can easily sort the recognition results and find the best fitting object.

--- vision/vision_object/src/recog.cpp
+++ vision/vision_object/src/recog.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <opencv2/core/core.hpp>
@@ -5,27 +6,64 @@
 #include <opencv2/nonfree/features2d.hpp>
 
 #include "recog.h"
+#include "train.h"
 
 using namespace cv;
 
-int recogObject(
-	Mat & descriptors,
-	ObjectTrainData_t & trainData
+typedef struct {
+	double meanError;
+	double meanSquareError;
+	double variance;
+} ObjectRecogScore_t;
+
+typedef struct {
+	ObjectTrainData_t * trainDataPtr;
+	ObjectRecogScore_t recogScore;
+} ObjectDataScorePair_t;
+
+bool ObjectDataScorePairPtrCompare(
+	ObjectDataScorePair_t * one,
+	ObjectDataScorePair_t * two
 );
 
 int recogObject(
 	Mat & descriptors,
-	ObjectTrainData_t & trainData
+	ObjectTrainData_t & trainData,
+	ObjectRecogScore_t & recogScore
+);
+
+/**
+* True if one is bettern than two
+*/
+bool ObjectDataScorePairPtrCompare(
+	ObjectDataScorePair_t * one,
+	ObjectDataScorePair_t * two
 ){
-	static FlannBasedMatcher matcher;
+	double errors[2];
+	errors[0] = one->recogScore.meanSquareError;
+	errors[1] = two->recogScore.meanSquareError;
+
+	if(errors[0] < errors[1]){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+int recogObject(
+	Mat & descriptors,
+	ObjectTrainData_t & trainData,
+	ObjectRecogScore_t & recogScore
+){
+	static BFMatcher matcher(NORM_L2);
 
 	/**
 	* Match descriptors
 	*/
-  	std::vector<DMatch> matches;
+	std::vector<DMatch> matches;
   	matcher.match(
-  		descriptors,
   		trainData.descriptors,
+  		descriptors,
   		matches
   	);
 
@@ -66,9 +104,20 @@ int recogObject(
   	double variance;
   	variance = meanSquareError - meanError * meanError;
 
+  	/**
+  	* Save score
+  	*/
+  	recogScore.meanError = meanError;
+  	recogScore.meanSquareError = meanSquareError;
+  	recogScore.variance = variance;
+
+  	/**
+  	* Show statistics
+  	*/
   	std::cout << "Statistics for ";
   	std::cout << trainData.file.object << std::endl;
-  	std::cout << " Mean: " << meanError << std::endl;
+  	std::cout << " Mean error: " << meanError << std::endl;
+  	std::cout << " Mean square error: " << meanSquareError << std::endl;
   	std::cout << " Variance " << variance << std::endl;
   	std::cout << std::endl;
 
@@ -103,9 +152,42 @@ int recog(
 	size_t numbTrainObjects;
 	numbTrainObjects = trainDataVect.size();
 
+	ObjectDataScorePair_t dataScorePairArr[numbTrainObjects];
+	ObjectDataScorePair_t * dataScorePairPtrArr[numbTrainObjects];
+
 	size_t i;
 	for(i = 0; i < numbTrainObjects; i++){
-		recogObject(image, trainDataVect[i]);
+		recogObject(
+			descriptors,
+			trainDataVect[i],
+			dataScorePairArr[i].recogScore
+		);
+
+		dataScorePairArr[i].trainDataPtr = &trainDataVect[i];
+		dataScorePairPtrArr[i] = &dataScorePairArr[i];
+	}
+
+	std::sort(
+		dataScorePairPtrArr,
+		dataScorePairPtrArr + numbTrainObjects,
+		ObjectDataScorePairPtrCompare
+	);
+
+	/**
+	* Show results
+	*/
+	std::cout << "RESULTS" << std::endl;
+	for(i = 0; i < numbTrainObjects; i++){
+		ObjectDataScorePair_t * pairPtr;
+		pairPtr = dataScorePairPtrArr[i];
+
+		std::cout << " Object: ";
+		std::cout << pairPtr->trainDataPtr->file.object;
+		std::cout << std::endl;
+
+		std::cout << " Mean square error: ";
+		std::cout << pairPtr->recogScore.meanSquareError;
+		std::cout << std::endl;
 	}
 
 	std::cout <<" End" << std::endl;
