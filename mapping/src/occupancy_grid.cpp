744fa65581a6ed839918294641b21632492dcc00
robo <robo@NUC.(none)>
2013-11-19 11:20:04 +0100

    First test of using ir sensors for mapping.

--- mapping/src/occupancy_grid.cpp
+++ mapping/src/occupancy_grid.cpp
@@ -9,6 +9,7 @@
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/MapMetaData.h>
 #include <control_logic/info.h>
+#include <core_sensors/ir.h>
 #include <vector>
 
 
@@ -16,14 +17,18 @@
 
  //Initialize
 const float PI=3.1415926f;
-const double resolution_matrix=0.02; // in meter!
-const int x_matrix=0.1/resolution_matrix; 
-const int y_matrix=0.1/resolution_matrix;
+const double resolution_matrix=0.01; // in meter!
+const int x_matrix=5/resolution_matrix; 
+const int y_matrix=5/resolution_matrix;
 
 // robot size: 2*delta_x+1, 2*delta_y+1
-const int robot_delta_x=5;
-const int robot_delta_y=5;
+const int robot_delta_x=10; // robot is a dot
+const int robot_delta_y=10;
 
+
+// sensor array
+
+double ir[8];
 
 int x_Current_Pose=round(x_matrix/2); //x,y coordinates
 int y_Current_Pose=round(y_matrix/2); //x,y coordinates
@@ -42,6 +47,7 @@ ros::Publisher map_pub;
 ros::Subscriber	odometry_sub;
 ros::Subscriber	camera_sub;
 ros::Subscriber	logic_sub;
+ros::Subscriber ir_sub;
 
 
 /* cell_round
@@ -58,7 +64,7 @@ double cell_round(double val){
 void Get_Readings_Odometry(nav_msgs::Odometry::ConstPtr odometry_msg){
 
 	double x_Pose_Odometry=cell_round(odometry_msg->pose.pose.position.x*100); //odometry is comming in meters
-	double y_Pose_Odometry=cell_round(odometry_msg->pose.pose.position.y*100);
+	double y_Pose_Odometry=-cell_round(odometry_msg->pose.pose.position.y*100);
 	
 	//Start in the middle of the grid
 	x_Current_Pose = (x_Pose_Odometry + round(x_matrix/2));
@@ -75,6 +81,23 @@ void place_map(int x_position, int y_position, int delta_x, int delta_y, int val
 
 		}
 	}
+	
+	
+}
+
+void place_ir(){
+	
+	
+	if(ir[4] < 20){
+		for(int i=0; i<cell_round(ir[4]);i++){
+			
+			place_map(x_Current_Pose+robot_delta_x,y_Current_Pose+robot_delta_y+i,0,0,0);
+			
+		}
+		
+		place_map(x_Current_Pose+robot_delta_x,y_Current_Pose+robot_delta_y+cell_round(ir[4]),0,0,100);
+	}
+	
 	
 	
 }
@@ -105,6 +128,14 @@ void Send_Message(){
 void Receive_Camera(int x_position, int y_position) {
 
 }
+
+void Get_Ir(core_sensors::ir::ConstPtr ir_msg){
+	
+	for(int i=0; i<8; i++)
+		ir[i]=ir_msg->dist[i];
+	
+}
+
 void Get_Motion_Info(control_logic::info::ConstPtr logic_msg) {
 	
 	wall=logic_msg->info_wall;
@@ -125,6 +156,7 @@ int main(int argc, char **argv)
     
 	odometry_sub = n.subscribe("/core_sensors_odometry/odometry",1,Get_Readings_Odometry);
 	logic_sub = n.subscribe("/control_logic/info",1,Get_Motion_Info);
+	ir_sub = n.subscribe("/core_sensors_ir/ir",1,Get_Ir);
 	
 	occ_pub = n.advertise<nav_msgs::OccupancyGrid>("/occupancy_grid/occ",1);
 	map_pub = n.advertise<nav_msgs::MapMetaData>("occupancy_grid/map",1);
@@ -136,9 +168,13 @@ int main(int argc, char **argv)
 
 	struct timeval start, end;
 	
+	for(int i=0; i<8; i++)
+		ir[i]=0.0;
+	
 	while(ros::ok()){
 
 		place_map(x_Current_Pose,y_Current_Pose,robot_delta_x,robot_delta_y,0);
+		place_ir();
 		Send_Message();
 
 		loop_rate.sleep();
