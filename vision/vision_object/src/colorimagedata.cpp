16e77f66e367be119dc54c421c5590df7d1b9cb3
Alessandro Motta <amotta91@gmail.com>
2013-11-21 14:05:12 +0100

    Use macro when reporting errors in color image module.

--- vision/vision_object/src/colorimagedata.cpp
+++ vision/vision_object/src/colorimagedata.cpp
@@ -115,14 +115,14 @@ int ColorImageData::train(const ColorImageContext & context){
 	int errorCode = 0;
 
 	if(path.empty()){
-		std::cout << "Error in ColorImageData::train" << std::endl;
+		std::cout << "Error in " << __FUNCTION__ << std::endl;
 		std::cout << "No path given" << std::endl;
 		return -1;
 	}
 
 	Mat image = imread(path, CV_LOAD_IMAGE_GRAYSCALE);
 	if(!image.data){
-		std::cout << "Error in ColorImageData::train" << std::endl;
+		std::cout << "Error in " << __FUNCTION__ << std::endl;
 		std::cout << "Could not read image" << std::endl;
 		std::cout << path << std::endl;
 		return -1;
